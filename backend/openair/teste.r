b27e1b3e155ff3b02417d66ea27fbbfd253f8430
Lucas Menezes <lucasmenezes4502@gmail.com>
2023-04-10 12:23:56 -0300

    testes com R

--- backend/openair/teste.r
+++ backend/openair/teste.r
@@ -1,7 +1,18 @@
+
+# IMPORTING LIBRARIES
 library(openair)
-library(tibble)
-mydata <- read.csv("C:\\Users\\lucassm\\.ArES\\temp\\XS_ST.csv",
-    header = TRUE,
-    na.strings = c("NA", "NaN", " ", "nan"))
+library(tidyverse)
+
+# READING AND PREPARING DATA
+mydata <- read_csv("C:\\Users\\lucassm\\.ArES\\temp\\SA_BS.csv",
+    na = c(" ", "nan"),
+    )
 mydata$date <- as.POSIXct(strptime(mydata$date, format = "%d/%m/%Y %H:%M",
 "GMT"))
+
+# CUSTOM COMMANDS NEEDS TO BE INSERTED HERE
+x <- summaryPlot(mydata)
+x$.Call.graphics()
+
+# OPTIONS TO SAVE FIGURE
+# ggsave("C:\\Users\\lucassm\\.ArES\\temp\\SA_BS.png", dpi = 200)
